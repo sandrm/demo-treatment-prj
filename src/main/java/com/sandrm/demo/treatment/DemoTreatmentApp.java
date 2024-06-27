a37dbed899bedae3cf93e0cf3da85d9a14f18e80
sandrm <sandrm@ukr.net>
2024-06-27 17:51:18 +0300

    initial commit

---
+++ src/main/java/com/sandrm/demo/treatment/DemoTreatmentApp.java
@@ -0,0 +1,13 @@
+package com.sandrm.demo.treatment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTreatmentApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTreatmentApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandrm/demo/treatment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandrm.demo.treatment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
