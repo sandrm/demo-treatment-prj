531f967b9dda84d735dc0c5db14160ccb052eb0c
sandrm <sandrm@ukr.net>
2024-07-01 01:02:43 +0300

    fixed if the plan is endless

--- src/main/java/com/sandrm/demo/treatment/repository/TreatmentPlanRepository.java
+++ src/main/java/com/sandrm/demo/treatment/repository/TreatmentPlanRepository.java
@@ -14,7 +14,7 @@ public interface TreatmentPlanRepository extends CrudRepository<TreatmentPlan, L
 
     @Query(value = """
             SELECT * FROM TREATMENT_PLAN tp
-            WHERE :treatment_date BETWEEN tp.START_TIME and tp.END_TIME""",
+            WHERE :treatment_date BETWEEN tp.START_TIME and tp.END_TIME and tp.END_TIME is not null""",
             nativeQuery = true)
     List<TreatmentPlan> getPlanForDate(@Param("treatment_date") Date treatment_date);
 }
